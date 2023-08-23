da1f6249fce141df394076ccf5a4c64dcebd6327
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-23 23:37:42 +0300

    src/application/orders package updated

---
+++ src/application/orders/__init__.py
@@ -0,0 +1,3 @@
+"""src/application/orders/__init__.py"""
+
+from src.application.orders.orders import *  # noqa: F401, F403, E501
---
+++ src/application/orders/orders.py
@@ -0,0 +1,14 @@
+"""src/application/orders.py"""
+
+from src.domain.orders import Order, OrdersRepository, OrderUncommited
+from src.domain.users import User
+from src.infrastructure.database.transaction import transaction
+
+
+@transaction
+async def create(payload: dict, user: User) -> Order:
+    payload.update(user_id=user.id)
+
+    order = await OrdersRepository().create(OrderUncommited(**payload))
+
+    return order
