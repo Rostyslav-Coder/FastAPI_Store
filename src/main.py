1075c5328d89a235156e34dca8f708905e49d5b0
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-23 11:11:44 +0300

    src/main.py updated

--- src/main.py
+++ src/main.py
@@ -1,5 +1,28 @@
 """src/main.py"""
 
 from fastapi import FastAPI
+from loguru import logger
 
+from src.config import settings
+
+# Adjust the logging
+# -------------------------------
+logger.add(
+    "".join(
+        [
+            str(settings.root_dir),
+            "/logs/",
+            settings.logging.file.lower(),
+            ".log",
+        ]
+    ),
+    format=settings.logging.format,
+    rotation=settings.logging.rotation,
+    compression=settings.logging.compression,
+    level="INFO",
+)
+
+
+# Adjust the application
+# -------------------------------
 app = FastAPI()
