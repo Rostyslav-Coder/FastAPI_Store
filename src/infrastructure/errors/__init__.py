9ec1b89a480dd697a4acb43f8b37e5a5e7df4ffd
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-23 15:13:22 +0300

    src/infrastructure/errors package updated

---
+++ src/infrastructure/errors/__init__.py
@@ -0,0 +1,4 @@
+"""src/infrastructure/errors/__init__.py"""
+
+from .base import *  # noqa: F401, F403
+from .handlers import *  # noqa: F401, F403
---
+++ src/infrastructure/errors/base.py
@@ -0,0 +1,98 @@
+"""src/infrastructure/errors/base.py"""
+
+# This module is responsible for describing shared errors
+# that are handled on the last step of processing the request.
+
+from typing import Any
+
+from starlette import status
+
+__all__ = (
+    "BaseError",
+    "BadRequestError",
+    "UnprocessableError",
+    "NotFoundError",
+    "DatabaseError",
+    "AuthenticationError",
+    "AuthorizationError",
+)
+
+
+class BaseError(Exception):
+    """Base Error class"""
+
+    def __init__(
+        self,
+        *_: tuple[Any],
+        message: str = "",
+        status_code: int = status.HTTP_500_INTERNAL_SERVER_ERROR,
+    ) -> None:
+        self.message: str = message
+        self.status_code: int = status_code
+
+        super().__init__(message)
+
+
+class BadRequestError(BaseError):
+    """Bad Request Error class"""
+
+    def __init__(self, *_: tuple[Any], message: str = "Bad request") -> None:
+        super().__init__(
+            message=message,
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+
+class UnprocessableError(BaseError):
+    """Unprocessable Error class"""
+
+    def __init__(
+        self, *_: tuple[Any], message: str = "Validation error"
+    ) -> None:
+        super().__init__(
+            message=message,
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        )
+
+
+class NotFoundError(BaseError):
+    """Not Found Error class"""
+
+    def __init__(self, *_: tuple[Any], message: str = "Not found") -> None:
+        super().__init__(
+            message=message, status_code=status.HTTP_404_NOT_FOUND
+        )
+
+
+class DatabaseError(BaseError):
+    """Database Error class"""
+
+    def __init__(
+        self, *_: tuple[Any], message: str = "Database error"
+    ) -> None:
+        super().__init__(
+            message=message, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+
+
+class AuthenticationError(BaseError):
+    """Authentication Error class"""
+
+    def __init__(
+        self, *_: tuple[Any], message: str = "Authentication error"
+    ) -> None:
+        super().__init__(
+            message=message,
+            status_code=status.HTTP_401_UNAUTHORIZED,
+        )
+
+
+class AuthorizationError(BaseError):
+    """Authorization Error class"""
+
+    def __init__(
+        self, *_: tuple[Any], message: str = "Authorization error"
+    ) -> None:
+        super().__init__(
+            message=message, status_code=status.HTTP_403_FORBIDDEN
+        )
