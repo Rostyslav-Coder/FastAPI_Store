039e2a7f03cb4703efafa704ca03f866c04ac98a
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-25 17:52:22 +0300

    src/application/authentication package updated

--- src/application/authentication/dependency_injection.py
+++ src/application/authentication/dependency_injection.py
@@ -1,6 +1,6 @@
 """src/application/authentication/dependency_injection.py"""
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
@@ -12,7 +12,10 @@ from src.domain.authentication import TokenPayload
 from src.domain.users import User, UsersRepository
 from src.infrastructure.errors import AuthenticationError
 
-__all__ = ("get_current_user",)
+__all__ = (
+    "get_current_user",
+    "create_access_token",
+)
 
 oauth2_oauth = OAuth2PasswordBearer(
     tokenUrl="/auth/openapi",
@@ -21,6 +24,8 @@ oauth2_oauth = OAuth2PasswordBearer(
 
 
 async def get_current_user(token: str = Depends(oauth2_oauth)) -> User:
+    """Function return current user"""
+
     try:
         payload = jwt.decode(
             token,
@@ -32,8 +37,24 @@ async def get_current_user(token: str = Depends(oauth2_oauth)) -> User:
         if datetime.fromtimestamp(token_payload.exp) < datetime.now():
             raise AuthenticationError
     except (JWTError, ValidationError):
-        raise AuthenticationError
+        raise AuthenticationError  # pylint: disable=W0707
 
     user = await UsersRepository().get(id_=token_payload.sub)
 
     return user
+
+
+def create_access_token(data: dict) -> str:
+    """function create & return access token"""
+
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(
+        seconds=settings.authentication.access_token.ttl
+    )
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.authentication.access_token.secret_key,
+        algorithm=settings.authentication.algorithm,
+    )
+    return encoded_jwt
