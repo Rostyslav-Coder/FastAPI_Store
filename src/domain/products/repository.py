d963b79611e5d876f5536dc27615297337be861c
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-26 19:35:18 +0300

    src/domain/products package updated

--- src/domain/products/repository.py
+++ src/domain/products/repository.py
@@ -19,6 +19,10 @@ class ProductRepository(BaseRepository[ProductsTable]):
         instance = await self._get(key="id", value=id_)
         return Product.from_orm(instance)
 
+    async def get_by_name(self, name_: str) -> Product:
+        instanse = await self._get(key="name", value=name_)
+        return Product.from_orm(instanse)
+
     async def create(self, schema: ProductUncommited) -> Product:
         instance: ProductsTable = await self._save(schema.dict())
         return Product.from_orm(instance)
