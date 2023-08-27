8ed2fa35a0787adb95a188b6f7f2b78e449cd741
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-27 23:45:55 +0300

    src/domain/products package updated

--- src/domain/products/repository.py
+++ src/domain/products/repository.py
@@ -1,6 +1,6 @@
 """src/domain/products/repository.py"""
 
-from typing import AsyncGenerator
+from typing import Any, AsyncGenerator
 
 from src.domain.products.models import Product, ProductUncommited
 from src.infrastructure.database import BaseRepository, ProductsTable
@@ -12,9 +12,9 @@ class ProductRepository(BaseRepository[ProductsTable]):
     schema_class = ProductsTable
 
     async def all(
-        self, skip: int = 0, limit: int = 5
+        self, skip_: int = 0, limit_: int = 5
     ) -> AsyncGenerator[Product, None]:
-        async for instance in self._all(skip=skip, limit=limit):
+        async for instance in self._all(skip=skip_, limit=limit_):
             yield Product.from_orm(instance)
 
     async def get(self, id_: int) -> Product:
@@ -25,6 +25,12 @@ class ProductRepository(BaseRepository[ProductsTable]):
         instanse = await self._get(key="name", value=name_)
         return Product.from_orm(instanse)
 
-    async def create(self, schema: ProductUncommited) -> Product:
-        instance: ProductsTable = await self._save(schema.dict())
+    async def create(self, schema_: ProductUncommited) -> Product:
+        instance: ProductsTable = await self._save(schema_.dict())
+        return Product.from_orm(instance)
+
+    async def update(
+        self, key_: str, value_: Any, payload_: dict[str, Any]
+    ) -> Product:
+        instance = await self._update(key=key_, value=value_, payload=payload_)
         return Product.from_orm(instance)
