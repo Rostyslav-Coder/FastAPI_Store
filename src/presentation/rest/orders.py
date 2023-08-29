f9b5959eba821cb30e3853ad131f59f928ea264c
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-29 23:42:55 +0300

    src/presentation/rest package updated

--- src/presentation/rest/orders.py
+++ src/presentation/rest/orders.py
@@ -3,6 +3,7 @@
 from fastapi import APIRouter, Depends, HTTPException, Request, status
 
 from src.application.authentication import get_current_user
+from src.domain.constants import OrderStatus
 from src.domain.orders import (
     Order,
     OrderCreateRequestBody,
@@ -10,7 +11,7 @@ from src.domain.orders import (
     OrdersRepository,
     OrderUncommited,
 )
-from src.domain.products import ProductRepository
+from src.domain.products import Product, ProductRepository
 from src.domain.users import User
 from src.infrastructure.database.transaction import transaction
 from src.infrastructure.models import Response, ResponseMulti
@@ -25,10 +26,12 @@ async def cart_create(
     schema: OrderCreateRequestBody,
     user: User = Depends(get_current_user),
 ) -> Response[OrderPublic]:
-    """Create a new pre-order."""
+    """Add product to cart"""
 
     # Get the product from database to check product`s amount
-    product = await ProductRepository().get(id_=schema.product_id)
+    product = await ProductRepository().get(
+        key_="id", value_=schema.product_id
+    )
     if product.amount < schema.amount:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -46,7 +49,7 @@ async def cart_create(
         delivery_address=user.address,
     )
 
-    # Save order to the database like in cart
+    # Add order to the database like in cart
     order: Order = await OrdersRepository().create(schema=order_raw)
     order_public = OrderPublic.from_orm(order)
 
@@ -57,8 +60,8 @@ async def cart_create(
 @transaction
 async def cart_list(
     _: Request,
-    skip: int,
-    limit: int,
+    skip: int = None,
+    limit: int = None,
     user: User = Depends(get_current_user),  # pylint: disable=W0613
 ) -> ResponseMulti[OrderPublic]:
     """Get all orders from my cart."""
@@ -66,7 +69,9 @@ async def cart_list(
     # Get all user`s products with 'PENDING' status from the database
     orders_public = [
         OrderPublic.from_orm(order)
-        async for order in OrdersRepository().all_my_catr(user.id, skip, limit)
+        async for order in OrdersRepository().all_pending(
+            key_="user_id", value_=user.id, skip_=skip, limit_=limit
+        )
     ]
 
     return ResponseMulti[OrderPublic](result=orders_public)
@@ -83,23 +88,23 @@ async def product_amount_update(
     """Update product amount"""
 
     # Get the order from database
-    order = await OrdersRepository().get(id_=order_id)
+    order = await OrdersRepository().get(key_="id", value_=order_id)
 
     # Check the status of the order
-    if order.status == "PENDING":
-        # Update products amount
-        payload = {"amount": new_amount}
-        product: Order = await OrdersRepository().update(
-            key_="id", value_=order_id, payload_=payload
-        )
-        product_public = OrderPublic.from_orm(product)
-
-        return Response[OrderPublic](result=product_public)
-    else:
+    if order.status != "PENDING":
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Order status is not pending",
         )
+
+    # Update products amount
+    payload = {"amount": new_amount}
+    product: Order = await OrdersRepository().update(
+        key_="id", value_=order_id, payload_=payload
+    )
+    product_public = OrderPublic.from_orm(product)
+
+    return Response[OrderPublic](result=product_public)
 
 
 @router.delete("/my_cart")
@@ -112,7 +117,7 @@ async def cart_remove(
     """Delete unpayed order from cart"""
 
     # Get the product from database to check product`s status
-    order = await OrdersRepository().get(id_=order_id)
+    order = await OrdersRepository().get(key_="id", value_=order_id)
     if order.status != "PENDING":
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -123,3 +128,63 @@ async def cart_remove(
     await OrdersRepository().delete(id_=order_id)
 
     return HTTPException(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@router.put("/pay_my_cart", status_code=status.HTTP_202_ACCEPTED)
+@transaction
+async def order_pay(
+    _: Request,
+    skip: int = None,
+    limit: int = None,
+    user: User = Depends(get_current_user),
+):
+    """Pay products from cart"""
+
+    # Creating products list from orders with status PENDING
+    product_list = [
+        Order.from_orm(order)
+        async for order in OrdersRepository().all_pending(
+            key_="user_id", value_=user.id, skip_=skip, limit_=limit
+        )
+    ]
+
+    # Update products amount in products database table
+    for order in product_list:
+        product: Product = await ProductRepository().get(
+            key_="id", value_=order.product_id
+        )
+        if product.amount >= order.amount:
+            product.amount -= order.amount
+            await ProductRepository().update(
+                key_="id",
+                value_=product.id,
+                payload_={"amount": product.amount},
+            )
+        else:
+            order.amount = product.amount
+            product.amount = 0
+            await OrdersRepository().update(
+                key_="id", value_=order.id, payload_={"amount": order.amount}
+            )
+            await ProductRepository().update(
+                key_="id",
+                value_=product.id,
+                payload_={"amount": product.amount},
+            )
+        # Update order status
+        await OrdersRepository().update(
+            key_="id", value_=order.id, payload_={"status": OrderStatus.PAID}
+        )
+
+    # Add updated orders id to list
+    orders_id = [order.id for order in product_list]
+
+    # Get updated orders
+    updated_orders = []
+    for order_id in orders_id:
+        order: Order = await OrdersRepository().get(key_="id", value_=order_id)
+        updated_orders.append(order)
+
+    orders_public = [OrderPublic.from_orm(order) for order in updated_orders]
+
+    return ResponseMulti[OrderPublic](result=orders_public)
