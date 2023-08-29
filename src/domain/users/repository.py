96c3cdb00bd9cf1ed7d9b3d9b1c76ff33a1e1f01
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-29 22:21:06 +0300

    src/domain/users package updated

--- src/domain/users/repository.py
+++ src/domain/users/repository.py
@@ -1,6 +1,6 @@
 """src/domain/users/repository.py"""
 
-from typing import AsyncGenerator
+from typing import Any, AsyncGenerator
 
 from src.domain.users.models import User, UserUncommited
 from src.infrastructure.database import BaseRepository, UsersTable
@@ -12,25 +12,23 @@ class UsersRepository(BaseRepository[UsersTable]):
     schema_class = UsersTable
 
     async def all(
-        self, skip: int = 0, limit: int = 5
+        self, skip_: int = None, limit_: int = None
     ) -> AsyncGenerator[User, None]:
-        async for instance in self._all(skip=skip, limit=limit):
+        async for instance in self._all(skip=skip_, limit=limit_):
             yield User.from_orm(instance)
 
-    async def get(self, id_: int) -> User:
-        instance = await self._get(key="id", value=id_)
-        return User.from_orm(instance)
-
-    async def get_by_email(self, email_: int) -> User:
-        instance = await self._get(key="email", value=email_)
+    async def get(self, key_: int, value_: Any) -> User:
+        instance = await self._get(key=key_, value=value_)
         return User.from_orm(instance)
 
     async def create(self, schema: UserUncommited) -> User:
         instance: UsersTable = await self._save(schema.dict())
         return User.from_orm(instance)
 
-    async def update(self, id_: int, schema: UserUncommited) -> User:
+    async def update(
+        self, key_: str, value_: Any, payload_: UserUncommited
+    ) -> User:
         instance: UsersTable = await self._update(
-            key="id", value=id_, payload=schema.dict()
+            key=key_, value=value_, payload=payload_.dict()
         )
         return User.from_orm(instance)
