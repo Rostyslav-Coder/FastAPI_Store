d1301a951be52ec458bf4c4ea323383c7a172818
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-29 22:47:53 +0300

    src/domain/products package updated

--- src/domain/products/repository.py
+++ src/domain/products/repository.py
@@ -12,18 +12,14 @@ class ProductRepository(BaseRepository[ProductsTable]):
     schema_class = ProductsTable
 
     async def all(
-        self, skip_: int = 0, limit_: int = 5
+        self, skip_: int = None, limit_: int = None
     ) -> AsyncGenerator[Product, None]:
         async for instance in self._all(skip=skip_, limit=limit_):
             yield Product.from_orm(instance)
 
-    async def get(self, id_: int) -> Product:
-        instance = await self._get(key="id", value=id_)
+    async def get(self, key_: str, value_: Any) -> Product:
+        instance = await self._get(key=key_, value=value_)
         return Product.from_orm(instance)
-
-    async def get_by_name(self, name_: str) -> Product:
-        instanse = await self._get(key="name", value=name_)
-        return Product.from_orm(instanse)
 
     async def create(self, schema_: ProductUncommited) -> Product:
         instance: ProductsTable = await self._save(schema_.dict())
@@ -36,4 +32,4 @@ class ProductRepository(BaseRepository[ProductsTable]):
         return Product.from_orm(instance)
 
     async def delete(self, id_: int) -> None:
-        return await super().delete(id_)
+        await self._delete(id_)
